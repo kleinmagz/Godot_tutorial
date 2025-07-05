ebaeeca81f7a6ba7a0c7fe466fa82bab16806de6
Klein Magallano <kleinalboriamagallano@gmail.com>
2025-07-05 17:57:01 +0800

    Added and modified a couple of scripts and scenes. Pretty much completed the beginner tutorial.

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -1,9 +1,11 @@
 extends Area2D
 @onready var timer: Timer = $Timer
+@onready var hurt_sound: AudioStreamPlayer2D = $HurtSound
 
 # body refers to the body that enters the area (only the player).
 # so we can reference to the player using body
 func _on_body_entered(body: Node2D) -> void:
+	hurt_sound.play()
 	print("You Died!")
 	Engine.time_scale = 0.5
 	body.get_node("CollisionShape2D").queue_free()
--- scripts/player.gd
+++ scripts/player.gd
@@ -4,6 +4,7 @@ extends CharacterBody2D
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var jump_sound: AudioStreamPlayer2D = $"../SFX/JumpSound"
 
 
 func _physics_process(delta: float) -> void:
@@ -14,6 +15,7 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		jump_sound.play()
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
